0b16da74dee4fd0b6007eab3098856a657c99552
Hikaru Hokkyokusei <kutabare.eerito@gmail.com>
2024-07-02 23:46:23 -0700

    Added manga request to get manga by id.

---
+++ MAL/manga.go
@@ -0,0 +1,31 @@
+package MyAnimeListSDK
+
+import (
+	"encoding/json"
+	"fmt"
+
+	MALModels "github.com/HikaruHokkyokusei/AnimeTree/MAL/Models"
+)
+
+var (
+	nilManga = MALModels.Manga{}
+)
+
+func (c MyAnimeListClient) GetManga(mangaId int64) (MALModels.Manga, error) {
+	resp, err := c.request(
+		GET, fmt.Sprintf("/manga/%d", mangaId),
+		map[string]string{
+			"fields": MALModels.AllMangaFields(),
+		}, "",
+	)
+	if err != nil {
+		return nilManga, err
+	}
+
+	if temp, err := json.Marshal(resp); err == nil {
+		manga := MALModels.Manga{}
+		return manga, json.Unmarshal(temp, &manga)
+	} else {
+		return nilManga, err
+	}
+}
